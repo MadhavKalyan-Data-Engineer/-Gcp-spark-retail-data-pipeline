49c312c87c916eb1ff63c6a95bc96a1d67ff362d
Madhav kalyan <92013369+MadhavKalyan-Data-Engineer@users.noreply.github.com>
2025-12-21 20:34:31 +0530

    Update project1_spark_dsl.py

--- scripts/src/project1_spark_dsl.py
+++ scripts/src/project1_spark_dsl.py
@@ -6,14 +6,13 @@ Project 1: PySpark DataFrame API
 - Add derived country column
 """
 
-create bucket kalyanspark
+create bucket myretail-prod-uscentral1-stg-orders
 copy linux files to the bucket 
-gsutil linux file path gs://kalyanspark/filename.json
-customer_lo_df=spark.read.format("json").load("gs://kalyanspark/filename.json")
+gsutil linux file path gs://myretail-prod-uscentral1-stg-orders/customer_lo.json
+customer_lo_df=spark.read.format("json").load("gs://myretail-prod-uscentral1-stg-orders/customer_lo.json")
 customer_filter_df=customer_lo_df.filter(customer_lo_df.grade==200)
 customer_filter_df.show()
 Customer_select_df=customer_filter_df.select("City","Cust_name","Customer_id")
 Customer_select_df.show()
-customer_select_df.write.format("json").mode("overwrite").save("/kalyanhdfs/customer_lo.json")
-
+customer_select_df.write.format("json").mode("overwrite").save("/myretail-prod-uscentral1-stg-orders/customer_lo.json")
 
