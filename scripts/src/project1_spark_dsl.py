fed0d7d1f33101533726ec48f57c81432f1b3ae9
Madhav kalyan <92013369+MadhavKalyan-Data-Engineer@users.noreply.github.com>
2026-03-02 09:32:58 +0530

    Update project1_spark_dsl.py for claims processing

--- scripts/src/project1_spark_dsl.py
+++ scripts/src/project1_spark_dsl.py
@@ -1,18 +1,31 @@
 """
 Project 1: PySpark DataFrame API
-- Read customer data from GCS
-- Union London & New York customers
-- Filter grade = 200
+- Read policyholder claims data from GCS
+- Union Mumbai & Delhi policyholders
+- Filter risk_score = 200
 - Add derived country column
 """
 
-create bucket myretail-prod-uscentral1-stg-orders
-copy linux files to the bucket 
-gsutil linux file path gs://myretail-prod-uscentral1-stg-orders/customer_lo.json
-customer_lo_df=spark.read.format("json").load("gs://myretail-prod-uscentral1-stg-orders/customer_lo.json")
-customer_filter_df=customer_lo_df.filter(customer_lo_df.grade==200)
-customer_filter_df.show()
-Customer_select_df=customer_filter_df.select("City","Cust_name","Customer_id")
-Customer_select_df.show()
-customer_select_df.write.format("json").mode("overwrite").save("/myretail-prod-uscentral1-stg-orders/customer_lo.json")
+# create bucket myinsurance-prod-uscentral1-stg-claims
+# copy linux files to the bucket 
+# gsutil cp /local/path/claims_mumbai.json gs://myinsurance-prod-uscentral1-stg-claims/claims_mumbai.json
+# gsutil cp /local/path/claims_delhi.json gs://myinsurance-prod-uscentral1-stg-claims/claims_delhi.json
 
+from pyspark.sql.functions import lit
+
+claims_mumbai_df = spark.read.format("json").load("gs://myinsurance-prod-uscentral1-stg-claims/claims_mumbai.json")
+claims_delhi_df = spark.read.format("json").load("gs://myinsurance-prod-uscentral1-stg-claims/claims_delhi.json")
+
+claims_union_df = claims_mumbai_df.union(claims_delhi_df)
+claims_union_df.show()
+
+claims_filter_df = claims_union_df.filter(claims_union_df.risk_score == 200)
+claims_filter_df.show()
+
+claims_select_df = claims_filter_df.select("city", "policyholder_name", "policy_id", "claim_amount")
+claims_select_df.show()
+
+claims_final_df = claims_select_df.withColumn("country", lit("India"))
+claims_final_df.show()
+
+claims_final_df.write.format("json").mode("overwrite").save("gs://myinsurance-prod-uscentral1-stg-claims/output/high_risk_claims.json")
